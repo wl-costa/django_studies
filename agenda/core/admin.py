2208acbed4ae98a8522a244ede751f790f8faae6
Wendell Costa <wendellcosta.dev@gmail.com>
2025-02-26 17:39:01 -0300

    model registrado

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+admin.site.register(Evento) # Registra o modelo Evento no painel de administração do Django
