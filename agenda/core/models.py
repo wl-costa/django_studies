d34bbdde6a3e24bcb46ec4b017bdfb1c26507da6
Wendell Costa <wendellcosta.dev@gmail.com>
2025-02-26 17:37:33 -0300

    foi criado um model evento e migrado para o banco de dados

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True) # A descrição pode ser nulo ou vazio
+    data_evento = models.DateTimeField()
+    data_criacao = models.DateTimeField(auto_now=True) # A data de criação do evento é automatica
+
+    class Meta:
+        db_table = 'evento' # Define o nome da tabela no banco de dados como 'evento' ao invés de 'core_evento' que é o padrão do Django
+    
