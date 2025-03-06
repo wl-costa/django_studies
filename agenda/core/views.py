d2f137088bdf65d42a68e9c8495ef399eb2d464d
Wendell Costa <wendellcosta.dev@gmail.com>
2025-03-06 17:46:15 -0300

    define um response para a função lista_eventos

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from core.models import Evento
 
 # Create your views here.
 
 def lista_eventos(request):
-    return render(request, 'agenda.html')
+    evento = Evento.objects.get(id=1)
+    response = {'evento': evento} # Cria um dicionário com o evento
+    return render(request, 'agenda.html', response)
