1edaec4a12661095428a8fb12e5c0bddac97a549
Wendell Costa <wendellcosta.dev@gmail.com>
2025-03-06 18:53:26 -0300

    remove o recurso de visualizar o evento apenas do usuário logado (testes serão feitos antes da implementação)

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -4,7 +4,7 @@ from core.models import Evento
 # Create your views here.
 
 def lista_eventos(request):
-    usuario = request.user # Pega o usuário logado no sistema e armazena na variável usuario 
-    evento = Evento.objects.filter(usuario=usuario) # Filtra os eventos do usuário logado e armazena na variável evento
+    # usuario = request.user # Pega o usuário logado no sistema e armazena na variável usuario 
+    evento = Evento.objects.all() # filter(usuario=usuario) # Filtra os eventos do usuário logado e armazena na variável evento
     dados = {'eventos': evento} # Cria um dicionário com o evento e armazena na variável dados
     return render(request, 'agenda.html', dados) # Renderiza a página agenda.html com os dados do dicionário
