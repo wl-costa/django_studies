93719b37cd0ec3d6c5107738ab0e5103f7feadf1
Wendell Costa <wendellcosta.dev@gmail.com>
2025-03-07 19:44:22 -0300

    agora o usuário vê apenas os seus agendamentos

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -24,7 +24,7 @@ def submit_login(request):
 
 @login_required(login_url='/login/') # Verifica se o usuário está logado, caso não esteja, redireciona para a página de login
 def lista_eventos(request):
-    # usuario = request.user # Pega o usuário logado no sistema e armazena na variável usuario 
-    evento = Evento.objects.all() # filter(usuario=usuario) # Filtra os eventos do usuário logado e armazena na variável evento
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario) # Filtra os eventos do usuário logado e armazena na variável evento
     dados = {'eventos': evento} # Cria um dicionário com o evento e armazena na variável dados
     return render(request, 'agenda.html', dados) # Renderiza a página agenda.html com os dados do dicionário
