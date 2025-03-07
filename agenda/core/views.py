bee399444da2c1a4c2a307ea5ab17118b2ec2b29
Wendell Costa <wendellcosta.dev@gmail.com>
2025-03-07 16:35:43 -0300

    cria a funçãi index que redireciona o índice da página para '/agenda/'

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect # Importa as funções render e redirect do módulo shortcuts
 from core.models import Evento
 
+
 # Create your views here.
+
+def index(request):
+    return redirect('/agenda/') # Redireciona para a página agenda.html
 
 def lista_eventos(request):
     # usuario = request.user # Pega o usuário logado no sistema e armazena na variável usuario 
