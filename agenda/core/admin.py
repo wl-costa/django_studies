607fc077f46a5713b5a425df46a5ec5f3bbe550b
Wendell Costa <wendellcosta.dev@gmail.com>
2025-03-10 16:17:56 -0300

    inclui o id no list display do admin

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -4,7 +4,7 @@ from core.models import Evento
 # Register your models here.
 
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'data_evento', 'data_criacao') # Exibe o título, data do evento e data de criação do evento no painel de administração do Django
+    list_display = ('id', 'titulo', 'data_evento', 'data_criacao') # Exibe o título, data do evento e data de criação do evento no painel de administração do Django
     list_filter = ('titulo', 'data_evento') # Adiciona um filtro por título e data do evento no painel de administração do Django
 
 admin.site.register(Evento, EventoAdmin) # Registra o modelo Evento no painel de administração do Django e aplica as configurações do EventoAdmin
