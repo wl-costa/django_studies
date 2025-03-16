b36acdfd92bda485f6c7d8ca1a8b4895674964a5
Wendell Costa <wendellcosta.dev@gmail.com>
2025-03-15 21:34:02 -0300

    Adiciona autenticação utilizando uma conta do google :)

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views
 from django.views.generic import RedirectView
 
@@ -31,4 +31,8 @@ urlpatterns = [
     path('logout/', views.logout_user),
     path('agenda/filter/', views.filter_eventos),
     path('agenda/search/', views.search_eventos),
+]
+
+urlpatterns += [
+    path('auth/', include('social_django.urls', namespace='social')),
 ]
